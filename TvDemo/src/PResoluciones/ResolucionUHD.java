2c348f839e723957e40a9eb8fa9ea2eb8f00306e
Jose Carlos Coronado <83930715+J-Carlos-Coronado@users.noreply.github.com>
2022-05-24 23:15:18 -0500

    Update ResolucionUHD.java

--- TvDemo/src/PResoluciones/ResolucionUHD.java
+++ TvDemo/src/PResoluciones/ResolucionUHD.java
@@ -1,19 +1,26 @@
 package PResoluciones;
 
+// @author: Jose Carlos Coronado Chable (Documentador)
+// @author:Alex Reyes (Coder)
+//@version: 24/05/2022
 //UHD res = 1920 x 1080
 /**
  * Class ResolucionHD
  */
+
 public class ResolucionUHD extends Resolucion {
-    
+    // Campos de tipo privado que tienen como variables estaticas largo y ancho que sirven para dar un tamaño a la pantalla
+    //Largo=1080 x ancho=1920
    protected final static double LARGO = 1080;
    protected final static double ANCHO = 1920;
-   
+   // Creacion del constructor Resolucion UHD que nos dara una mejor visivilidad en la tv 
+
+   //Super toma como parametros el largo y el ancho 
    public ResolucionUHD(){
        super(LARGO,ANCHO);
    }
 }
-
+//Cierre de la clase y del metodo
 
 
 
